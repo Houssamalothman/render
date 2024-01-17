c7fc83f3b63b9a52ed72d0f4204924b4db96a8f4
unknown <alothman.randa@gmail.com>
2024-01-17 22:21:42 +0200

    added a new endpoint /data

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ const PORT = process.env.PORT || 3030;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+app.get('/data', function (req, res) {
+var data = [
+    {id:17, name:'omar'},
+    {id:23, name:'houssam'},
+    {id:29, name:'walid'}
+]
+    res.send(data)
+})
 app.listen(PORT)
